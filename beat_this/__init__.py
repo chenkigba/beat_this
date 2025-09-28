ef99c4879f1fa5e44e8881d940c72840d82ba917
Wuji Chen <chenwuji2000@gmail.com>
2025-09-28 19:34:22 +0800

    fix

--- beat_this/__init__.py
+++ beat_this/__init__.py
@@ -1,2 +1,13 @@
 from .inference import preload, setup, File2File, File2Beats, Audio2Beats, Audio2Frames, Spect2Frames, load_model, load_checkpoint
 
+__all__ = [
+    "preload",
+    "setup",
+    "File2File",
+    "File2Beats",
+    "Audio2Beats",
+    "Audio2Frames",
+    "Spect2Frames",
+    "load_model",
+    "load_checkpoint",
+]
